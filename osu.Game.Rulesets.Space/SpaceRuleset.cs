6bc4d49abb9cec088b53c4e299d4209e5cdb4ac5
michioxd <michio.haiyaku@gmail.com>
2026-03-09 23:07:05 +0700

    2026.309.2

--- osu.Game.Rulesets.Space/SpaceRuleset.cs
+++ osu.Game.Rulesets.Space/SpaceRuleset.cs
@@ -29,7 +29,7 @@ namespace osu.Game.Rulesets.Space
     {
         public override string Description => "osu!space";
         public override string ShortName => "osuspaceruleset";
-        public const string VERSION_STRING = "2026.309.1";
+        public const string VERSION_STRING = "2026.309.2";
         public SpaceRuleset()
         {
             SpaceLegacyBeatmapDecoder.Register();
