8ec5ae4642e1709644232678e679dda831162f64
michioxd <michio.haiyaku@gmail.com>
2026-07-07 04:23:37 +0700

    remove copyright comments from localisation files

--- osu.Game.Rulesets.Space/Localisation/Mods/SpaceModNoScopeStrings.cs
+++ osu.Game.Rulesets.Space/Localisation/Mods/SpaceModNoScopeStrings.cs
@@ -1,6 +1,3 @@
-// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
 using osu.Framework.Localisation;
 
 namespace osu.Game.Rulesets.Space.Localisation.Mods
